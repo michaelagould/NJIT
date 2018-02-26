90d0a0363b3d1e73b3399f904c9092bc1ce1b062
Michael Gould Admin <michaelgould@localhost.localdomain>
2018-02-25 22:59:17 -0500

    cleaning

--- CS-288/Homework 3/stupid.sh
+++ CS-288/Homework 3/stupid.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-i=0
-j=0
-let i++
+intList=(0 0 0 0 0 0 0 0 0 0)
 
-echo $i
+for file in $(ls $1)
+do
+	i=0
+	while [ $i -lt ${#intList[@]} ]
+	do
+	if [ $file = $i* ]
+		let intList[$i]=${intList[$i]}+1
+	let i++
+	#case $file in
+	#		$i*)
+	#	let intList[$i]=${intList[$i]}+1
+	#		;;
+	#esac
+	#let i++
+	done
+done
+
+i=0
+
+while [ $i -lt ${#intList[@]} ]
+do
+	echo $i ${intList[$i]}
+	let i++
+done
