d571fefc1dfe49455fa26d20768ab95f29636a95
mike <michael.a.gould@gmail.com>
2018-03-02 00:40:21 -0500

    finished with program 2

--- CS-506/Programming Projects/MichaelGould_Program2.c
+++ CS-506/Programming Projects/MichaelGould_Program2.c
@@ -2,97 +2,34 @@
 #include <stdlib.h>
 
 int count;
-int i;
-int sample;
 
-struct Stack
-{
-    int top;
-    int capacity;
-    int* array;
-    char name;
-    int fakeTop;
-};
-
-struct Stack* createStack(char name, int capacity)
-{
-    struct Stack* stack = malloc(sizeof(struct Stack));
-    stack->name = name;
-    stack->capacity = capacity;
-    stack->top = 0;
-    stack->fakeTop = stack->top;
-    stack->array = (int*) malloc(stack->capacity * sizeof(int));
-    return stack;
-}
-
-void push(struct Stack* stack, int item){
-    if (stack->top == stack->capacity)
-        return;
-    stack->array[++stack->top] = item;
-    stack->fakeTop = stack->top;
-}
-
-int pop(struct Stack* stack){
-    if (stack->top == 0)
-        return -1;
-    return stack->array[stack->top--];
-}
-
-int printPop(struct Stack* stack){
-    if (stack->fakeTop == 0)
-        return -1;
-    return stack->array[stack->fakeTop--];
-}
-
-void printState(struct Stack* A, struct Stack* B, struct Stack* C){
+void moveAmountStartEndUsing(int n, char A, char B, char C){
 	
-	while(A->fakeTop != 0)
-		printf("%d ", printPop(A));
-	printf("      ");
-	while(B->fakeTop != 0)
-		printf("%d ", printPop(B));
-	printf("      ");
-	while(C->fakeTop != 0)
-		printf("%d ", printPop(C));
-	printf("      ");
-	A->fakeTop = A->top;
-	B->fakeTop = B->top;
-	C->fakeTop = C->top;
-}
-
-void moveAmountStartEndUsing(int amount, struct Stack* A, struct Stack* B, struct Stack* C){
-	
-	if(amount == 1){
+	if(n == 1){
 		count++;
-		printf("\n%d Move disc %d from %c to %c", count, amount, A->name, B->name);
+		printf("\n%d Move disc %d from %c to %c", count, n, A, B);
 	}
 	else{
-		moveAmountStartEndUsing(amount, A, C, B);
+		moveAmountStartEndUsing(n-1, A, C, B);
 		count++;
-		printf("\n%d Move disk %d from %c to %c", count, amount, A->name, B->name);
-		moveAmountStartEndUsing(amount, C, B, A);
+		printf("\n%d Move disk %d from %c to %c", count, n, A, B);
+		moveAmountStartEndUsing(n-1, C, B, A);
 		}
 }
 
 int main(int argc, char *argv[]){
 
-	int inputSize = atoi(argv[1]);
+	int n = atoi(argv[1]);
 
-	struct Stack *A = createStack('A', inputSize + 1);
-	struct Stack *B = createStack('B', inputSize + 1);
-	struct Stack *C = createStack('C', inputSize + 1);
-
-	for(i = inputSize; i > 0; i--){
-		push(A, i);
+	if(n > 6){
+		printf("Input too large, quitting program\n");
+		return 0;
 	}
-	
+
+	else{
 	printf("Tower of Hanoi Solver: \n");
-	printf("Input number of disks (max 6): %d\n", inputSize);
-	printf("Inital State:");
-	printState(A, B, C);
-	getchar();
-	moveAmountStartEndUsing(inputSize, A, B, C);
-	
+	printf("Input number of disks (max 6): %d", n);
+	moveAmountStartEndUsing(n, 'A', 'B', 'C');
 	printf("\n");
-	printState(A, B, C);
+	}
 }
---
+++ CS-506/Programming Projects/Program2Stacks.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int count;
+int i;
+int sample;
+
+struct Stack
+{
+    int top;
+    int capacity;
+    int* array;
+    char name;
+    int fakeTop;
+};
+
+struct Stack* createStack(char name, int capacity)
+{
+    struct Stack* stack = malloc(sizeof(struct Stack));
+    stack->name = name;
+    stack->capacity = capacity;
+    stack->top = 0;
+    stack->fakeTop = stack->top;
+    stack->array = (int*) malloc(stack->capacity * sizeof(int));
+    return stack;
+}
+
+void push(struct Stack* stack, int item){
+    if (stack->top == stack->capacity)
+        return;
+    stack->array[++stack->top] = item;
+    stack->fakeTop = stack->top;
+}
+
+int pop(struct Stack* stack){
+    if (stack->top == 0)
+        return -1;
+    return stack->array[stack->top--];
+}
+
+int printPop(struct Stack* stack){
+    if (stack->fakeTop == 0)
+        return -1;
+    return stack->array[stack->fakeTop--];
+}
+
+void printState(struct Stack* A, struct Stack* B, struct Stack* C){
+	
+	printf("A:");
+	while(A->fakeTop != 0)
+		printf("%d ", printPop(A));
+	printf("  B:");
+	while(B->fakeTop != 0)
+		printf("%d ", printPop(B));
+	printf("  C:");
+	while(C->fakeTop != 0)
+		printf("%d ", printPop(C));
+	printf("      ");
+	A->fakeTop = A->top;
+	B->fakeTop = B->top;
+	C->fakeTop = C->top;
+}
+
+void moveAmountStartEndUsing(int amount, struct Stack* A, struct Stack* B, struct Stack* C){
+	
+	if(amount == 1){
+		count++;
+		printf("\n%d Move disc %d from %c to %c", count, amount, A->name, B->name);
+	}
+	else{
+		moveAmountStartEndUsing(amount-1, A, C, B);
+		count++;
+		printf("\n%d Move disk %d from %c to %c", count, amount, A->name, B->name);
+		moveAmountStartEndUsing(amount-1, C, B, A);
+		}
+}
+
+int main(int argc, char *argv[]){
+
+	int inputSize = atoi(argv[1]);
+
+	struct Stack *A = createStack('A', inputSize + 1);
+	struct Stack *B = createStack('B', inputSize + 1);
+	struct Stack *C = createStack('C', inputSize + 1);
+
+	for(i = inputSize; i > 0; i--){
+		push(A, i);
+	}
+	
+	printf("Tower of Hanoi Solver: \n");
+	printf("Input number of disks (max 6): %d\n", inputSize);
+	printf("Inital State: ");
+	printState(A, B, C);
+	moveAmountStartEndUsing(inputSize, A, B, C);
+	printf("\nEnd State: ");
+	printState(B, A, C);
+	printf("\n");
+}
