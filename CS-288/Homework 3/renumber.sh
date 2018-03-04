4ecfcc85f9da87a7a54ca27119b22ef3235256b1
Michael Gould Admin <michaelgould@localhost.localdomain>
2018-03-04 12:43:42 -0500

    final commit, helping mike

--- CS-288/Homework 3/renumber.sh
+++ CS-288/Homework 3/renumber.sh
@@ -3,15 +3,24 @@ directoryName='./test' ## destination folder
 fileString=$(ls $directoryName) ## string of contents from destination folder path
 fileList=($fileString)
 index=0
-
-for file in ${fileList[@]}
-do
-done
-
+exception=()
 
 for file in ${fileList[@]}
 do
 	if [ -e $directoryName/$1$(printf %03d $index).$2 ]
+	then
+		exception=(${exception[@]} + ${fileList[index]})
+		let index++
+	else
+		let index++
+	fi
+done
+
+index=0
+
+for file in ${fileList[@]}
+do
+	if [ $directoryName/$1$(printf %03d $index).$2 = ${exception[$index]} ]
 	then
 		let index++
 	else 
