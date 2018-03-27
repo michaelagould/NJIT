f1858e792532c6574458b9d14966818344e3c1bf
michaelagould <michael.a.gould@gmail.com>
2018-03-26 23:35:25 -0400

    push from midterm 2

--- CS-288/QuizPreps/insert.sh
+++ CS-288/QuizPreps/insert.sh
@@ -3,13 +3,10 @@ array=($@)
 for (( i = 1; i < ${#array[@]}; i++ )); 
 do
 	key=${array[$i]}
-	let "j = $i - 1" #this is weird
-	while [[ j -gt 0 && array[$j] > key ]]; 
+	for (( j = $i-1; j >= 0 && array[$j] > key; j-- )); 
 	do
-		array[$(j+1)]=${array[$j]}
-		j=$j-1
+		array[j+1]=${array[$j]}
 	done
-	array[(($j+1))]=$key
+	array[$j+1]=$key
 done
-
 echo ${array[@]}
