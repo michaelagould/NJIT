befd70f1a55e3f15b2356ab40dd23301f8a66276
Mike <michaelagould@gmail.com>
2018-03-28 14:26:55 -0400

    merge sort additions

---
+++ CS-288/mergesort.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int* split(int* sample, int l, int r){
+	if (sizeof(sample[0]) == sizeof(sample))
+		return sample;
+	else{
+		leftIndex = 
+		rightIndex
+		middle = samplesize/2
+		split(sample, )
+		split(sample/(middle+1))
+		merge(part1, part2)
+	}
+
+int main() {
+	 
+}
