5afb603b5ed2debb2144fa9b782982ef7d45aaf6
Mike <michaelagould@gmail.com>
2018-04-24 14:17:58 -0400

    Have to use tage names, no Id

--- CS-288/Homework 7/extract.py
+++ CS-288/Homework 7/extract.py
@@ -1,2 +1,14 @@
 import sys
 import xml.dom.minidom
+
+document = xml.dom.minidom.parse(sys.argv[1])
+tableElements = document.getElementsByTagName('h1')
+
+
+for tr in tableElements[0].getElementsByTagName('tr'):
+	data = []
+	for td in tr.getElementsByTagName('td'):
+		for node in td.childNodes:
+			if node.nodeType == node.TEXT_NODE:
+				data.append(node.nodeValue)
+	print(','.join(data))
