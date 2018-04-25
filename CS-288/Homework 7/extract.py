ed99fd8fcbfd796479a2e0f982e26c9e7922bb2a
Mike <michaelagould@gmail.com>
2018-04-25 16:59:41 -0400

    finished with HW7

--- CS-288/Homework 7/extract.py
+++ CS-288/Homework 7/extract.py
@@ -1,19 +1,23 @@
 import sys, xml.dom.minidom
 import csv
 
-with open('output.csv', 'a') as f:
-	writer = csv.writer(f)
+filename = "output" + sys.argv[2] + ".csv"
+
+with open(filename, 'a') as f:
 	
 	document = xml.dom.minidom.parse(sys.argv[1])
 	heading1_Elements = document.getElementsByTagName('h1')
 	heading4_Elements = document.getElementsByTagName('h4')
-	bold_Elements = document.getElementsByTagName('b')
+	td_Elements = document.getElementsByTagName('td')
 
-	state = sys.argv[2]
-	city = sys.argv[3]
-	weather = heading4_Elements[2].childNodes[0].nodeValue
-	temperature = heading1_Elements[1].childNodes[0].nodeValue
-	humidity = bold_Elements[3].childNodes[0].nodeValue
-	pressure = bold_Elements[5].childNodes[0].nodeValue
-	
-	writer.writerow([state, city, weather, temperature, humidity, pressure])
+	state = sys.argv[3]
+	city = sys.argv[4]
+	i = 5
+	for args in range(i, len(sys.argv)):
+		city = city + " " + sys.argv[i]
+		i= i + 1
+	weather = heading4_Elements[2].childNodes[0].nodeValue.encode('ascii', 'ignore')
+	temperature = heading1_Elements[1].childNodes[0].nodeValue.encode('ascii', 'ignore')
+	humidity = td_Elements[1].childNodes[0].nodeValue.encode('ascii', 'ignore')
+	pressure = td_Elements[5].childNodes[0].nodeValue.encode('ascii', 'ignore')
+	f.write(",".join([state, city, weather, temperature[:4], humidity[:4], pressure[33:38].strip()]) + "\n")
--- CS-288/Homework 7/main.sh
+++ CS-288/Homework 7/main.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-StateList=(NY 'New York City' 40.78 -73.97 
-CA 'Los Angeles' 34.02 -118.45 
+a=1
+while [ true ]
+do
+
+StateList=(NY "New York City" 40.78 -73.97 
+CA "Los Angeles" 34.02 -118.45 
 IL Chicago 41.78 -87.76 
 TX Houston 29.64 -95.28 
 AZ Phoenix 33.69 -112.07 
 PA Philadelphia 40.08 -75.01 
 FL Jacksonville 30.23 -81.67 )
+
+echo state, city, weather, temperature, humidity, pressure >> output$a.csv
 
 for((i = 0; i < ${#StateList[@]}; i = i + 4)){
 	state=${StateList[$i]};
@@ -16,6 +22,9 @@ for((i = 0; i < ${#StateList[@]}; i = i + 4)){
 	output=$(date +"%F-%I-%M-%S")-$state
 	wget -O $output.html https://forecast-v3.weather.gov/point/$lat,$long?view=plain
 	java -jar tagsoup-1.2.1.jar --files $output.html
-	python extract.py $output $state $city
-	rm $output.html #$output.xhtml 
+	python extract.py $output.xhtml $a $state $city
+	rm $output.html $output.xhtml 
 }
+(( a++ ))
+sleep 60
+done
