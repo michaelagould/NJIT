45c643524eb7c337268a16bb04b3d4528b271310
michaelagould <michael.a.gould@gmail.com>
2018-04-24 23:00:02 -0400

    ascii values not good for csv write

--- CS-288/Homework 7/extract.py
+++ CS-288/Homework 7/extract.py
@@ -1,14 +1,19 @@
-import sys
-import xml.dom.minidom
+import sys, xml.dom.minidom
+import csv
 
-document = xml.dom.minidom.parse(sys.argv[1])
-tableElements = document.getElementsByTagName('h1')
+with open('output.csv', 'a') as f:
+	writer = csv.writer(f)
+	
+	document = xml.dom.minidom.parse(sys.argv[1])
+	heading1_Elements = document.getElementsByTagName('h1')
+	heading4_Elements = document.getElementsByTagName('h4')
+	bold_Elements = document.getElementsByTagName('b')
 
-
-for tr in tableElements[0].getElementsByTagName('tr'):
-	data = []
-	for td in tr.getElementsByTagName('td'):
-		for node in td.childNodes:
-			if node.nodeType == node.TEXT_NODE:
-				data.append(node.nodeValue)
-	print(','.join(data))
+	state = sys.argv[2]
+	city = sys.argv[3]
+	weather = heading4_Elements[2].childNodes[0].nodeValue
+	temperature = heading1_Elements[1].childNodes[0].nodeValue
+	humidity = bold_Elements[3].childNodes[0].nodeValue
+	pressure = bold_Elements[5].childNodes[0].nodeValue
+	
+	writer.writerow([state, city, weather, temperature, humidity, pressure])
--- CS-288/Homework 7/main.sh
+++ CS-288/Homework 7/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-StateList=(NY NewYork 40.78 -73.97 
-CA LosAngeles 34.02 -118.45 
+StateList=(NY 'New York City' 40.78 -73.97 
+CA 'Los Angeles' 34.02 -118.45 
 IL Chicago 41.78 -87.76 
 TX Houston 29.64 -95.28 
 AZ Phoenix 33.69 -112.07 
@@ -13,8 +13,9 @@ for((i = 0; i < ${#StateList[@]}; i = i + 4)){
 	city=${StateList[$i+1]}
 	lat=${StateList[$i+2]}
 	long=${StateList[$i+3]}
-	output=$(date +"%Y-%m-%d-%l-%M-%S")
-	wget -O $output-$state.html https://forecast-v3.weather.gov/point/$lat,$long?view=plain
-	java -jar tagsoup-1.2.1.jar --files $output-$state.html
-	rm $output-$state.html
+	output=$(date +"%F-%I-%M-%S")-$state
+	wget -O $output.html https://forecast-v3.weather.gov/point/$lat,$long?view=plain
+	java -jar tagsoup-1.2.1.jar --files $output.html
+	python extract.py $output $state $city
+	rm $output.html #$output.xhtml 
 }
