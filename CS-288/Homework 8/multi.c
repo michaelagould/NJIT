31f7bbfc806ebe76050f1c10954cae7ab4b7afe9
Mike <michaelagould@gmail.com>
2018-05-08 13:38:09 -0400

    home stretch

---
+++ CS-288/Homework 8/multi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+	
+	int array[argv[1]][argv[1]];
+		for(i = 0; i < argv[1]; i++){
+			for(j = i; J < argv[1]; j++)
+				array[i][j] = rand();
+		}
+	
+
+
+
+}
+
+void init();
